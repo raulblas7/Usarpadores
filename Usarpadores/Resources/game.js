53dfa3b9d3109b942d68e1bed30b83f864dcbefb
raulblas7 <raulblas@ucm.es>
2019-10-18 14:56:11 +0200

    carga de imagen añadida con fisicas

--- Usarpadores/Resources/game.js
+++ Usarpadores/Resources/game.js
@@ -1,13 +1,21 @@
+
+
 export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: 'main' });
   }
   preload() {  
+  	this.load.image("muñeco", "favicon.png");
   }
 
   create() {
+  	this.muñeco = this.physics.add.image(100,100,"muñeco");
+  	this.muñeco.setScale(5);
+  	//Fisicas
+  	this.muñeco.setCollideWorldBounds(true); //limita el personaje a los limites del mundo
   }
 
-  update(time, delta) {    
+  update(time, delta) {   
+
   }
 }
