0165dec2cdccfb7caba46a28a7fffd522648d66e
raulblas7 <raulblas@ucm.es>
2019-10-18 18:46:48 +0200

    implementaciones en game.js

--- Usarpadores/Resources/game.js
+++ Usarpadores/Resources/game.js
@@ -5,17 +5,28 @@ export default class Game extends Phaser.Scene {
     super({ key: 'main' });
   }
   preload() {  
-  	this.load.image("muñeco", "favicon.png");
+  	this.load.image("terreno", "images/terreno.png");
+  	this.load.image("gladiador", "images/gladiador1.png");
+  	this.load.image("muro", "images/muro.png");
+  	this.load.image("jugador", "images/jugador1.png");
   }
 
   create() {
-  	this.muñeco = this.physics.add.image(100,100,"muñeco");
-  	this.muñeco.setScale(5);
+  	this.terreno = this.add.image(0,0, "terreno");
+  	this.muro = this.add.image(87.5,225, "muro");
+  	this.gladiador = this.physics.add.image(600,385,"gladiador");
+  	this.jugador = this.add.image(87.5,225, "jugador");
+  	this.terreno.setScale(7);
+  	this.jugador.setScale(0.5);
+  	this.gladiador.setScale(0.8);
   	//Fisicas
-  	this.muñeco.setCollideWorldBounds(true); //limita el personaje a los limites del mundo
+  	this.gladiador.setCollideWorldBounds(true); //limita el personaje a los limites del mundo
+  	this.gladiador.body.gravity.y = 0; //corregir
+  	this.gladiador.setVelocity(-10,0);
+
   }
 
   update(time, delta) {   
-
+  	
   }
 }
