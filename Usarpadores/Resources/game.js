2c23e46c869db53f6e1d893ae5693fe44bedfbc1
raulblas7 <raulblas@ucm.es>
2019-10-19 18:37:09 +0200

    añadido un nuevo enemigo

--- Usarpadores/Resources/game.js
+++ Usarpadores/Resources/game.js
@@ -5,28 +5,42 @@ export default class Game extends Phaser.Scene {
     super({ key: 'main' });
   }
   preload() {  
+
   	this.load.image("terreno", "images/terreno.png");
+  	//hacer sprites
   	this.load.image("gladiador", "images/gladiador1.png");
-  	this.load.image("muro", "images/muro.png");
   	this.load.image("jugador", "images/jugador1.png");
+  	this.load.image("guerrero", "images/Guerrero1.png");
+  	this.load.image("lancer", "images/lancer1.png");
+
+
+  	this.load.image("muro", "images/muro.png");
   }
 
   create() {
   	this.terreno = this.add.image(0,0, "terreno");
   	this.muro = this.add.image(87.5,225, "muro");
-  	this.gladiador = this.physics.add.image(600,385,"gladiador");
+  	this.gladiador = this.physics.add.image(600,415,"gladiador");
+  	this.guerrero = this.physics.add.sprite(600, 315, "guerrero");
+  	this.lancer = this.physics.add.sprite(600, 215, "lancer");
   	this.jugador = this.add.image(87.5,225, "jugador");
   	this.terreno.setScale(7);
   	this.jugador.setScale(0.5);
   	this.gladiador.setScale(0.8);
+  	this.guerrero.setScale(0.8);
+  	this.lancer.setScale(0.8);
   	//Fisicas
   	this.gladiador.setCollideWorldBounds(true); //limita el personaje a los limites del mundo
-  	this.gladiador.body.gravity.y = 0; //corregir
-  	this.gladiador.setVelocity(-10,0);
+  	this.guerrero.setCollideWorldBounds(true);
+  	this.lancer.setCollideWorldBounds(true);
+  	
+  	this.gladiador.setVelocity(-15,0);
+  	this.guerrero.setVelocity(-30,0);
+  	this.lancer.setVelocity(-20,0);
 
   }
 
   update(time, delta) {   
-  	
+
   }
 }
