4d3a6e1624deaf8dcdaae2f48c137b7297771bec
raulblas7 <raulblas@ucm.es>
2019-10-25 18:45:56 +0200

    sprites añadidos con exito y movimiento (fallan colisiones con el muro)

--- Usarpadores/Resources/game.js
+++ Usarpadores/Resources/game.js
@@ -1,8 +1,8 @@
-//import Enemies from 'sprites.js';
+import Sprites from './sprites.js';
 
 export default class Game extends Phaser.Scene {
   constructor() {
-    super({ key: 'main' });
+    super({ key: 'Game' });
   }
 
   preload() {  
@@ -10,6 +10,7 @@ export default class Game extends Phaser.Scene {
   	this.load.image("terreno", "images/terreno.png");
   	//hacer sprites
   	this.load.image("gladiador", "images/gladiador1.png");
+    this.load.image("gladiador2", "images/gladiador1.png");
   	this.load.image("jugador", "images/jugador1.png");
   	this.load.image("guerrero", "images/Guerrero1.png");
   	this.load.image("lancer", "images/lancer1.png");
@@ -20,28 +21,45 @@ export default class Game extends Phaser.Scene {
 
   create() {
   	this.terreno = this.add.image(0,0, "terreno");
-  	this.muro = this.add.image(87.5,225, "muro");
-  	this.gladiador = this.physics.add.image(600,415,"gladiador");
-  	this.guerrero = this.physics.add.sprite(600, 315, "guerrero");
-  	this.lancer = this.physics.add.sprite(600, 215, "lancer");
-  	this.jugador = this.add.image(87.5,225, "jugador");
+    this.muro = new Sprites(this, 87.5, 225, "muro");
+    this.gladiador = new Sprites(this, 600, 415, "gladiador");
+    this.gladiador2 = new Sprites(this, 600, 115, "gladiador");
+    this.guerrero = new Sprites(this, 600, 315, "guerrero");
+    this.lancer = new Sprites(this, 600, 215, "lancer");
+  	this.jugador = new Sprites(this,87.5,225, "jugador");
+
   	this.terreno.setScale(7);
   	this.jugador.setScale(0.5);
   	this.gladiador.setScale(0.8);
+    this.gladiador2.setScale(0.8);
   	this.guerrero.setScale(0.8);
   	this.lancer.setScale(0.8);
-  	//Fisicas
-  	this.gladiador.setCollideWorldBounds(true); //limita el personaje a los limites del mundo
-  	this.guerrero.setCollideWorldBounds(true);
-  	this.lancer.setCollideWorldBounds(true);
-  	
-  	this.gladiador.setVelocity(-15,0);
-  	this.guerrero.setVelocity(-30,0);
-  	this.lancer.setVelocity(-20,0);
 
+    this.gladiador.body.setVelocity(-10,0);
+    this.gladiador2.body.setVelocity(-10,0);
+    this.guerrero.body.setVelocity(-15,0);
+    this.lancer.body.setVelocity(-12,0);
+
+    this.cursor = this.input.keyboard.createCursorKeys();
+    //colision enemigos y muro
+    this.physics.add.collider(this.muro, this.gladiador, this.collision(this.gladiador), null, this);
+    this.physics.add.collider(this.muro, this.gladiador2, this.collision(this.gladiador2), null, this);
+    this.physics.add.collider(this.muro, this.guerrero, this.collision(this.guerrero), null, this);
+    this.physics.add.collider(this.muro, this.lancer, this.collision(this.lancer), null, this);
   }
 
   update(time, delta) {   
+    //control del jugador
+    if(this.cursor.down.isDown){
+      this.jugador.body.setVelocityY(100);
+    }
+    if(this.cursor.up.isDown){
+      this.jugador.body.setVelocityY(-100);
+    }
+    
 
+  }
+  collision(objeto){
+    this.objeto.body.setVelocity(0);
   }
 }
--- Usarpadores/Resources/sprites.js
+++ Usarpadores/Resources/sprites.js
@@ -1,8 +1,10 @@
-class Sprites extends Phaser.GameObjects.Enemies{
+export default class Sprites extends Phaser.GameObjects.Sprite{
 	constructor(scene, x, y, type){
 		super(scene, x, y, type);
 		scene.add.existing(this);
+		scene.physics.world.enable(this);
+		this.body.immovable = true;
+		this.body.setCollideWorldBounds(true);
 	}
 }
 
-export default Sprites;
